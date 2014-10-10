6a3afaf7c20ed1de08d5aec34718cf4a1a4b4f70
ljiangyarris <lei.jiang@yarris.com>
2014-10-11 10:08:11 +1100

    update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.engine('jade', require('jade').__express);
 
 app.use('/public' , express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function() {
+app.set('port', (process.env.PORT || 5000));
+
+var server = app.listen(app.get('port'), function() {
     console.log('Listening on port %d', server.address().port);
 });
 
